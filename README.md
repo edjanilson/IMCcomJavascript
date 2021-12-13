d54d11833cc54217a1e8bc3c267fd1115961e78d
Eduardo Programador <edumfariabraga@gmail.com>
2021-12-12 23:53:53 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Programa que calcula o IMC (Índice de massa corpórea) usando HTML, CSS e Javascript.
 Programa feito com HTML, CSS e Javascript que permite calcular o IMC de uma pessoa, bastando para isso informar seu nome, altura e peso. O programa calculará seu IMC dividindo seu peso pela altura 
 elevada ao quadrado.
+
+# Funcionalidade de IAC implementada
+Foi adicionado o Índice de Adiposidade Corporal (IAC),
+uma medida utilizada para calcular a taxa de gordura em um ser humano,
+complementando ainda mais o projeto.
--- imc.js
+++ imc.js
@@ -5,8 +5,9 @@ function imc () {
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
+    const quadril = document.getElementById('quadril').value;
 
-    if (nome !== '' && altura !== '' && peso !== '') {
+    if (nome !== '' && altura !== '' && peso !== '' && quadril !== '') {
 
         const valorIMC = (peso / (altura * altura)).toFixed(1);
 
@@ -26,10 +27,34 @@ function imc () {
             classificacao = 'com obesidade grau III. Cuidado!!';
         }
 
-        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+        const valorIAC = (quadril / (altura * altura)).toFixed(1);
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao} Seu IAC é de ${valorIAC}. Para homens, o ideal é entre 8 a 20 e, para mulheres, de 21 a 32.`;
+        
+    } else if (nome !== '' && altura !== '' && peso !== '') {
+
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = '';
+
+        if (valorIMC < 18.5){
+            classificacao = 'abaixo do peso.';
+        }else if (valorIMC < 25) {
+            classificacao = 'com peso ideal. Parabéns!!!';
+        }else if (valorIMC < 30){
+            classificacao = 'levemente acima do peso.';
+        }else if (valorIMC < 35){
+            classificacao = 'com obesidade grau I.';
+        }else if (valorIMC < 40){
+            classificacao = 'com obesidade grau II';
+        }else {
+            classificacao = 'com obesidade grau III. Cuidado!!';
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao} Da próxima vez, informe a circunferência do seu quadril para calcular o IAC.`;
         
     }else {
-        resultado.textContent = 'Preencha todos os campos!!!';
+        resultado.textContent = 'Preencha todos os campos. Apenas o campo quadril não é obrigatório!!!';
     }
 
 }
@@ -43,6 +68,7 @@ function limpa() {
 document.getElementById("nome").value=" "
 document.getElementById("peso").value=" "
 document.getElementById("altura").value=" "
+document.getElementById("quadril").value=" "
 resultado.textContent=" ";
 
 }
